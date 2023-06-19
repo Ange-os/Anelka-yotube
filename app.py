1dbe948e3dfe99627d5217e8e8131a7d313881b6
angel <mather@.com>
2023-06-19 17:56:33 -0300

    version video final

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import *
 from pytube import YouTube
 import os
 import re
+import threading
 
 app = Flask(__name__)
 
+app.secret_key = 'tu_clave_secreta_aqui'
 
-path = os.getcwd() + '/output/'
+path = os.path.join(app.root_path, 'output')
 
 @app.route('/')
 def index():
@@ -20,16 +22,23 @@ def download():
             yt = YouTube(url)
             title = yt.title
             filename = re.sub(r'[^\w\s-]', '', title) + '.mp4'
-            p = path + title + '.mp4'
+            filepath = os.path.join(path, filename)
             video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            print('hasta aqui llega')
             video.download(path)
-            return send_file(p, as_attachment=True)
-            redirect(url_for('index.html')) 
+            threading.Timer(3, delete_files, args=[path]).start()
+            return send_file(filepath, as_attachment=True)
         except Exception as e:
             flash('Error al descargar el video: ' + str(e))
     else:
         flash('URL no válida')
+    return redirect(url_for('index')) 
 
+def delete_files(folder):
+    for file in os.listdir(folder):
+        file_path = os.path.join(folder, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 if __name__ == '__main__':
     app.run()
---
+++ script.js
@@ -0,0 +1,5 @@
+let formulario = document.getElementById('formu')
+
+formulario.addEventListener('submit', () => {
+    window.location.href = '/download.html'
+})
